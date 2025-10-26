11e028e22d9d67f85fda264dc01bc209832ca8af
2017kartik <kartikagarwal9119063420@gmail.com>
2025-10-26 21:52:00 +0530

    Add user to Stream

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,23 @@
+import { StreamChat } from "stream-chat";
+import "dotenv/config";
+
+const apiKey = process.env.STEAM_API_KEY;
+const apiSecret = process.env.STEAM_API_SECRET;
+
+if (!apiKey || !apiSecret) {
+  throw new Error(
+    "Stream API key and secret must be set in environment variables."
+  );
+}
+
+const streamClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const upsertStreamUser = async (userData) => {
+  try {
+    await streamClient.upsertUsers([userData]);
+    return userData;
+  } catch (error) {
+    console.log("Error upserting stream user: ", error);
+  }
+};
+ 
