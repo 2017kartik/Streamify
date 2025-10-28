fa6bdd1e89ffa49f7bf22876bc200686ff5f1a46
2017kartik <kartikagarwal9119063420@gmail.com>
2025-10-28 17:38:48 +0530

    Add Protected Route and Update User in Mongo DB and Stream feature.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -114,3 +114,60 @@ export function logout(req, res) {
   });
   return res.status(200).json({ message: "Logout successful." });
 }
+
+export async function onboard(req, res) {
+  try {
+    const userId = req.user._id;
+    const { fullName, bio, nativeLanguage, learningLanguage, location } =
+      req.body;
+
+    if (!fullName || !bio || !nativeLanguage || !learningLanguage || !location)
+      return res.status(400).json({
+        message: "All fields are required",
+        missingFields: [
+          !fullName && "fullName",
+          !bio && "bio",
+          !nativeLanguage && "nativeLanguage",
+          !learningLanguage && "learningLanguage",
+          !location && "location",
+        ].filter(Boolean),
+      });
+
+    const updateUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        fullName,
+        bio,
+        nativeLanguage,
+        learningLanguage,
+        location,
+        isOnboarded: true,
+      },
+      { new: true }
+    );
+
+    if (!updateUser) return res.status(404).json({ message: "User not found" });
+
+    try {
+      await upsertStreamUser({
+        id: updateUser._id.toString(),
+        name: updateUser.fullName,
+        image: updateUser.profilePic || "",
+      });
+
+      console.log(
+        `Stream user updated after onboarding for ${updateUser.fullName}`
+      );
+    } catch (streamError) {
+      console.log(
+        "Error updating stream user during onboarding: ",
+        streamError.message
+      );
+    }
+
+    return res.status(200).json({ success: true, user: updateUser });
+  } catch (error) {
+    console.error("Onboarding error: ", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { login, logout, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { onboard } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/sign-up", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.post("/onboarding", protectRoute, onboard);
 
 export default router;
